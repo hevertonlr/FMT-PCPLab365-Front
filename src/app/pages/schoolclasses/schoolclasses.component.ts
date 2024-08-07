ff4de2b147a64346578bd485be488c17e2b63690
Heverton L. Roieski <hevertonlr@gmail.com>
2024-08-07 00:38:01 -0300

    feat: pages, routes and icons

---
+++ src/app/pages/schoolclasses/schoolclasses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schoolclasses',
+  standalone: true,
+  imports: [],
+  templateUrl: './schoolclasses.component.html',
+  styleUrl: './schoolclasses.component.scss',
+})
+export class SchoolClassesComponent {}
